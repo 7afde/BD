e086c1fc54732caf87da3444285ee842ee8d3b3c
hafed <hafed.zouyed@univ-constantine2.dz>
2024-02-28 23:57:07 +0100

    I added all

---
+++ test.sql
@@ -0,0 +1,15 @@
+create database tp1;
+use tp1;
+
+
+create table groupes(codeGrp int(2),
+        annee varchar(5) not null,
+        constraint pk_groupes primary key (codeGrp)
+);
+
+create table students(matricule varchar(10),
+        nom varchar(30) not null,
+        grp int(2),
+        constraint pk_students primary key (matricule),
+        constraint fk_students foreign key (grp) references groupes(codeGrp)
+);
